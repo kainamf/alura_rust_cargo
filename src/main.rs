155a0de2331c80cd07fd50052549b54474bd587d
KainaMF <kaina.marques@calindra.com.br>
2024-03-20 14:09:05 -0300

    Arrays - Aula:  multiplos valores

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let notas: [f32; 4] = [10.0, 8.0, 9.5, 6.0];
+
+    println!(
+        "Nota 1 = {}, Nota2 = {}, Nota3 = {}, Nota 4={}",
+        notas[0],
+        notas[1],
+        notas[2],
+        notas[3]
+    );
 }
