010022674665017e94d40375ee9099640f9f7b2c
KainaMF <kaina.marques@calindra.com.br>
2024-03-21 13:11:53 -0300

    Conclusão

--- src/main.rs
+++ src/main.rs
@@ -123,12 +123,20 @@ struct Conta {
     saldo: f64
 }
 
+impl Conta {
+    fn sacar(&mut self, valor: f64) {
+        self.saldo -= valor;
+    }
+}
+
 fn conta_corrente() {
     let titular = Titular{nome: String::from("Vinicius"), sobrenome: String::from("Dias")};
-    let conta: Conta = Conta{
+    let mut conta: Conta = Conta{
         titular,
         saldo: 100.0
     };
+
+    conta.sacar(50.0);
 
     println!("Dados da conta: Titular = {} {}, Saldo = {}",
     conta.titular.nome,
